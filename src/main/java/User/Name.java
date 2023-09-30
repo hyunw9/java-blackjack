803c4301c13a1821b6f14506c2fc40c22669a051
hyunw9 <rkdgusdnr32@naver.com>
2023-09-30 17:26:50 +0900

    feat: Name 정적 팩토리 메서드

--- src/main/java/User/Name.java
+++ src/main/java/User/Name.java
@@ -4,7 +4,13 @@ public class Name {
 
     String name;
 
-    public Name(final String name) {
+    public Name(String name) {
         this.name = name;
     }
+
+    public static Name createName(String name){
+        validateWrongNameValue(name);
+        return new Name(name);
+    }
+
 }
