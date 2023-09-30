81e0447926038b9c8c6aa7257bc7995b9476a51f
hyunw9 <rkdgusdnr32@naver.com>
2023-09-30 17:26:00 +0900

    Test: Name 테스트

--- src/test/java/User/NameTest.java
+++ src/test/java/User/NameTest.java
@@ -1,9 +1,9 @@
 package User;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 
 class NameTest {
@@ -19,4 +19,27 @@ class NameTest {
         //then
         assertThat(testName.name).isEqualTo("홍길동");
     }
+
+    @Test
+    public void 이름_생성_이름값_NULL(){
+        //given
+        String nameLine = null;
+
+        //when,then
+        assertThatThrownBy(()-> Name.createName(nameLine))
+                .hasMessageContaining("이름은 Null 값이 될 수 없습니다.")
+                .isInstanceOf(NullPointerException.class);
+    }
+
+    @Test
+    public void 이름_생성_길이_검증(){
+        //given
+        String nameLine= "";
+
+        //when,then
+        assertThatThrownBy(()->Name.validateNameLength(nameLine))
+                .hasMessageContaining("이름 길이는 1글자 이상이어야 합니다.")
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
+
